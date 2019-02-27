896a15f3f8a964bd843c1ae83069b23ecdcb6874
kduong@stuy.edu <kduong@stuy.edu>
2019-02-26 21:41:07 -0500

    Added to file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello, Github");
+    String s = "";
+    s += "New commit!";
+    System.out.println(s);
+    for (int i = 0; i < 10; i ++) {
+      System.out.println(i);
+    }
   }
 }
